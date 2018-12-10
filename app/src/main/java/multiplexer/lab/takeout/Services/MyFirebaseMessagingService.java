c5373f45f31a3d9d00c4cd51ff70487f38ce6c2d
MultiplexerLab <multiplexer.lab@gmail.com>
2018-12-11 00:49:35 +0600

    Notification channel added

--- app/src/main/java/multiplexer/lab/takeout/Services/MyFirebaseMessagingService.java
+++ app/src/main/java/multiplexer/lab/takeout/Services/MyFirebaseMessagingService.java
@@ -1,33 +1,108 @@
 package multiplexer.lab.takeout.Services;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.widget.RemoteViews;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.squareup.picasso.Picasso;
 
-public class MyFirebaseMessagingService extends FirebaseMessagingService{
+import java.util.Map;
 
-        @Override
-        public void onMessageReceived(RemoteMessage remoteMessage) {
-            Log.d("Dhurr", "From: " + remoteMessage.getFrom());
+import multiplexer.lab.takeout.MainActivity;
+import multiplexer.lab.takeout.R;
 
-            if (remoteMessage.getData().size() > 0) {
-                Log.d("Dhurr", "Message data payload: " + remoteMessage.getData());
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
-                if (/* Check if data needs to be processed by long running job */ true) {
-                    // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
-                    //scheduleJob();
-                } else {
-                    // Handle message within 10 seconds
-                    // handleNow();
+    private String channel = "Takeout Offer";
+    private NotificationManager notificationManager;
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            setupChannels();
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            showNotifications(remoteMessage);
+        }
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
+
+    private void showNotifications(final RemoteMessage remoteMessage) {
+        Log.d("Dhurr2", remoteMessage.getNotification().getTitle());
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        final RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.notification_image_only);
+
+        remoteViews.setTextViewText(R.id.notificationTitle, remoteMessage.getNotification().getTitle());
+        remoteViews.setTextViewText(R.id.notificationDes, remoteMessage.getNotification().getBody());
+        final int notificationId = 1459756;
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(getApplicationContext(), channel)
+                .setSmallIcon(R.drawable.logo)
+                .setContentTitle(remoteMessage.getNotification().getTitle())
+                .setContentText(remoteMessage.getNotification().getBody())
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .setCustomBigContentView(remoteViews)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                .setSound(defaultSoundUri)
+                .setStyle(new NotificationCompat.DecoratedCustomViewStyle());
+
+        final Notification notification = notificationBuilder.build();
+        Handler uiHandler = new Handler(Looper.getMainLooper());
+        uiHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Picasso.with(MyFirebaseMessagingService.this)
+                            .load(remoteMessage.getNotification().getLink())
+                            .into(remoteViews, R.id.imageLarge, notificationId, notification);
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
+        });
 
-            // Check if message contains a notification payload.
-            if (remoteMessage.getNotification() != null) {
-                Log.d("Dhurr", "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        notificationManager.notify(notificationId, notification);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void setupChannels() {
+        try {
+            NotificationChannel adminChannel;
+            adminChannel = new NotificationChannel(channel, "Ramadan Eid OFFER", NotificationManager.IMPORTANCE_HIGH);
+            adminChannel.setDescription("50% Offer in Every Products");
+            adminChannel.enableLights(true);
+            adminChannel.setLightColor(Color.RED);
+            adminChannel.enableVibration(true);
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(adminChannel);
             }
-            // Also if you intend on generating your own notifications as a result of a received FCM
-            // message, here is where that should be initiated. See sendNotification method below.
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
 }
